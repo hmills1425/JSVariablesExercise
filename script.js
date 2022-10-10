1ca8ea214a09d9bb4953212d7fc795c4ea842934
Heather Mills <heathermills1980@gmail.com>
2022-10-10 07:34:56 -0500

    Completed Exercise

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1
+// const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+// 4
+num +=7;
+num = num + 7
+// console.log(num);
+
+// 5
+num --;
+
+// 6
+num = "This is not a number";
+// console.log(num);
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+
+// BONUS
+// 1
+// const quarter = 0.25, dime = 0.10, nickel = 0.5, penny = 0.01;
+
+const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+// 2
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letters = "qwertyyuiop"
+
+// 4
+console.log(letters[letters.length - 1]);
+
